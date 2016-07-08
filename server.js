f2ba9095a6bb909c720bed37de5c58816901a583
Nicolas Gotchac <ngotchac@gmail.com>
2016-07-08 12:06:37 +0100

    Slack integration

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-var express = require('express');
+var express = require('express'),
+    bodyParser = require('body-parser');
 
 var Database = require('./database');
 
@@ -7,6 +8,7 @@ const PORT = process.env.PORT || 3000;
 var db = new Database();
 
 var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/fromages', (req, res) => {
     res.send(getFromages());
@@ -17,6 +19,27 @@ app.get('/fromage', (req, res) => {
     var idx = Math.floor(Math.random() * fromages.length);
 
     res.send(fromages[idx]);
+});
+
+app.post('/get-fromage', (req, res) => {
+    var fromages = getFromages();
+    var idx = Math.floor(Math.random() * fromages.length);
+
+    var fromage = fromages[idx];
+    var token = req.body.token;
+
+    res.send({
+        attachments: [{
+            title: fromage.title,
+            title_link: fromage.link,
+            text: fromage.description,
+            fields: fromage.details.map(d => ({
+                title: d.key,
+                value: d.value
+            })),
+            image_url: 'https:' + fromage.image
+        }]
+    });
 });
 
 function getFromages() {
